91022cd00bc0ac727a5ff2fcf16a220b34c85376
Vinicius Santos <viniciusantos0898@gmail.com>
2023-07-27 12:27:19 -0300

    Added a hardcoded countdown logic; changed the screen style

--- app/src/main/java/com/example/savethedateapp/ui/MainScreen.java
+++ app/src/main/java/com/example/savethedateapp/ui/MainScreen.java
@@ -2,65 +2,110 @@ package com.example.savethedateapp.ui;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.os.CountDownTimer;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.savethedateapp.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MainScreen#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class MainScreen extends Fragment {
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class MainScreen extends AppCompatActivity {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private Button startButton;
+    private TextView daysTextView;
+    private TextView monthsTextView;
+    private TextView yearsTextView;
+    private TextView hoursTextView;
+    private TextView minutesTextView;
+    private TextView secondsTextView;
+    private CountDownTimer countDownTimer;
 
-    public MainScreen() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MainScreen.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MainScreen newInstance(String param1, String param2) {
-        MainScreen fragment = new MainScreen();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
+        //recupera o content view
+        setContentView(R.layout.fragment_main_screen);
+
+        startButton = findViewById(R.id.btn_start_count);
+        yearsTextView = findViewById(R.id.tv_countdown_year);
+        monthsTextView = findViewById(R.id.tv_countdown_month);
+        daysTextView = findViewById(R.id.tv_countdown_days);
+        hoursTextView = findViewById(R.id.tv_countdown_hours);
+        minutesTextView = findViewById(R.id.tv_countdown_minutes);
+        secondsTextView = findViewById(R.id.tv_countdown_seconds);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                StartCountdown();
+            }
+        });
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_screen, container, false);
+    private void StartCountdown(){
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String targetDateString = "2023-08-13 22:15:00";
+
+        try {
+            Date targetDateTime = dateFormat.parse(targetDateString);
+            long targetDateTimeInMilis = targetDateTime.getTime();
+
+            long currDateTimeInMilis = System.currentTimeMillis();
+            long countdownMilis = targetDateTimeInMilis - currDateTimeInMilis;
+
+            if(countDownTimer != null){
+                countDownTimer.cancel();
+            }
+
+            countDownTimer = new CountDownTimer(countdownMilis, 1000) {
+                @Override
+                public void onTick(long l) {
+                    yearsTextView.setText(formatTime(l, 'y'));
+                    monthsTextView.setText(formatTime(l, 'M'));
+                    daysTextView.setText(formatTime(l, 'd'));
+                    hoursTextView.setText(formatTime(l, 'h'));
+                    minutesTextView.setText(formatTime(l, 'm'));
+                    secondsTextView.setText(formatTime(l, 's'));
+                    //setar os textos
+                    //setar a visibility
+                }
+
+                @Override
+                public void onFinish() {
+                    //setar o header text view para contagem encerrada!
+                    //tirar a visibility dos textviews
+                }
+            };
+            countDownTimer.start();
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    private String formatTime(long milis, char type){
+
+        switch(type){
+            case 'y': return String.format("%d", TimeUnit.MILLISECONDS.toDays(milis)/ 365);
+            case 'M': return String.format("%d", TimeUnit.MILLISECONDS.toDays(milis) / 30);
+            case 'd': return String.format("%d", TimeUnit.MILLISECONDS.toDays(milis) % 30);
+            case 'h': return String.format("%02d", TimeUnit.MILLISECONDS.toHours(milis) % 24);
+            case 'm': return String.format("%02d", TimeUnit.MILLISECONDS.toMinutes(milis) % 60);
+            case 's': return String.format("%02d", TimeUnit.MILLISECONDS.toSeconds(milis) % 60);
+            default: return "";
+        }
+
     }
 }
